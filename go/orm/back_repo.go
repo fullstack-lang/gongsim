9f779e31e24e3416d7180739168dc881b5acea73
peugeot <thomaspeugeot@yahoo.fr>
2024-10-25 04:52:58 +0200

    new gongc

--- go/orm/back_repo.go
+++ go/orm/back_repo.go
@@ -13,9 +13,7 @@ import (
 	"github.com/fullstack-lang/gongsim/go/db"
 	"github.com/fullstack-lang/gongsim/go/models"
 
-	/* THIS IS REMOVED BY GONG COMPILER IF TARGET IS gorm
 	"github.com/fullstack-lang/gongsim/go/orm/dbgorm"
-	THIS IS REMOVED BY GONG COMPILER IF TARGET IS gorm */
 
 	"github.com/tealeg/xlsx/v3"
 )
@@ -50,9 +48,10 @@ func NewBackRepo(stage *models.StageStruct, filename string) (backRepo *BackRepo
 
 	var db db.DBInterface
 
+	/* THIS IS REMOVED BY GONG COMPILER IF TARGET IS lite
 	db = NewDBLite()
+	THIS IS REMOVED BY GONG COMPILER IF TARGET IS lite */
 
-	/* THIS IS REMOVED BY GONG COMPILER IF TARGET IS gorm
 	db = dbgorm.NewDBWrapper(filename, "github_com_fullstack_lang_gongsim_go",
 		&DummyAgentDB{},
 		&EngineDB{},
@@ -61,7 +60,6 @@ func NewBackRepo(stage *models.StageStruct, filename string) (backRepo *BackRepo
 		&GongsimStatusDB{},
 		&UpdateStateDB{},
 	)
-	THIS IS REMOVED BY GONG COMPILER IF TARGET IS gorm */
 
 	backRepo = new(BackRepoStruct)
 
@@ -327,7 +325,7 @@ func (backRepoStruct *BackRepoStruct) broadcastNbCommitToBack() {
 			activeChannels = append(activeChannels, ch)
 		default:
 			// Assume channel is no longer active; don't add to activeChannels
-			log.Println("Channel no longer active")
+			log.Println("github.com/fullstack-lang/gongsim/go: Channel no longer active", backRepoStruct.stage.GetPath())
 			close(ch)
 		}
 	}
