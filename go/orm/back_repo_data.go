ffea6880c937d60ba94a22ec8ca744e3194edf19
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2024-11-07 18:33:47 +0100

    new gongc

--- go/orm/back_repo_data.go
+++ go/orm/back_repo_data.go
@@ -18,6 +18,11 @@ type BackRepoData struct {
 }
 
 func CopyBackRepoToBackRepoData(backRepo *BackRepoStruct, backRepoData *BackRepoData) {
+
+	// wait till backRepo is written by commit
+	backRepo.rwMutex.RLock()
+	defer backRepo.rwMutex.RUnlock()
+
 	// insertion point for slices copies
 	for _, dummyagentDB := range backRepo.BackRepoDummyAgent.Map_DummyAgentDBID_DummyAgentDB {
 
